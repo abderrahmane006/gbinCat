a4cf1605968f8c8fa706ef847dc93900275fa0e8
Stéphane Lopes <stephane.lopes@prism.uvsq.fr>
2015-03-19 12:13:26 +0100

    feat(tools): extrait les accesseurs d'une classe

---
+++ src/main/java/adam/gaia/ExtractGetter.java
@@ -0,0 +1,35 @@
+package adam.gaia;
+
+import gaia.cu1.mdb.cu3.auxdata.igsl.dm.IgslSource;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by hal on 19/03/15.
+ */
+public class ExtractGetter {
+    public static void main(String... args) {
+        List<String> methodNames = new ArrayList<>();
+        Method[] allMethods = IgslSource.class.getDeclaredMethods();
+        for (Method m : allMethods) {
+            String methodName = m.getName();
+            if (methodName.startsWith("get")) {
+                String attribute = methodName.substring(3);
+                methodNames.add(attribute);
+            }
+        }
+        Collections.sort(methodNames);
+        for (String methodName : methodNames) {
+            System.out.println(" * " + methodName);
+        }
+//        for (String methodName : methodNames) {
+//            System.out.println("if (attribute.equals(\"" + methodName + "\")) {");
+//            System.out.println("    outData[attributeIdx] = String.valueOf(igslData.get" + methodName + "());");
+//            System.out.println("}");
+//            System.out.println("else");
+//        }
+    }
+}
