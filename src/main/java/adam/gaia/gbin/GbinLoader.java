75bad4c1338e6e8c99f54df1c6b814b636c0a460
Stéphane Lopes <stephane.lopes@prism.uvsq.fr>
2015-06-25 11:38:20 +0200

    refactor(gbin): apporte des modifications mineures

--- src/main/java/adam/gaia/gbin/GbinLoader.java
+++ src/main/java/adam/gaia/gbin/GbinLoader.java
@@ -3,17 +3,17 @@ package adam.gaia.gbin;
 import gaia.cu1.tools.dal.ObjectFactory;
 import gaia.cu1.tools.dal.file.FileStore;
 import gaia.cu1.tools.dal.table.GaiaTable;
+import gaia.cu1.tools.dm.GaiaRoot;
 import gaia.cu1.tools.exception.GaiaException;
 import gaia.cu1.tools.util.props.PropertyLoader;
 import org.apache.commons.io.FilenameUtils;
 
 import java.nio.file.Path;
 
-
 /**
  * Lit et charge un fichier gbin.
  */
-public class GbinLoader<V> {
+public class GbinLoader {
 	/**
 	 * Accès au fichier gbin.
 	 */
@@ -22,42 +22,30 @@ public class GbinLoader<V> {
 	/**
 	 * Type de source de données
 	 */
-	private Class sourceType;
+	private Class<?> sourceType;
 
 	/**
 	 * Initialise l'accès au fichier gbin.
 	 *
-	 * @param sourceType le type de données stockées dans le fichier (IgslSource.class, CatalogueSource.class)
+	 * @param sourceType le type de données stockées dans le fichier (IgslSource.class, CatalogueSource.class, ...)
 	 * @throws GaiaException
 	 */
-	public GbinLoader(Class sourceType) throws GaiaException {
-		PropertyLoader.load(); // TODO : à quoi cela sert-il ?
+	public GbinLoader(Class<?> sourceType) throws GaiaException {
+		PropertyLoader.load(); // nécéssaire pour initialiser certaines propriétés (http://gaia.esac.esa.int/GaiaTools/api/latest/gaia/cu1/tools/util/props/PropertyLoader.html)
 		store = new FileStore();
 		this.sourceType = sourceType;
 	}
-	
+
 	/**
 	 * Charge les données.
+	 *
 	 * @return un tableau contenant les données
 	 * @throws GaiaException
 	 */
-	public V[] loadData(Path file) throws GaiaException {
+	public GaiaRoot[] loadData(Path file) throws GaiaException {
 		GaiaTable gt = store.getFile(file.getParent().toString(),
 				FilenameUtils.getBaseName(file.getFileName().toString()));
-		ObjectFactory<V> of = new ObjectFactory<V>(sourceType);
+		ObjectFactory<GaiaRoot> of = new ObjectFactory<>(sourceType);
 		return of.getObjects(gt);
 	}
-
-//	private Object[] loadDataForProjection(GaiaTable gt, List<String> projList) throws GaiaException {
-//		GaiaTableHeader gth = gt.getHeader();
-//		List<String> attributeList = projList.equals(Collections.emptyList()) ?
-//				gth.getColumnNames() : projList;
-//		ArrayList<Object> data = new ArrayList<>();
-//		while (gt.next()) {
-//			for (String attribute : projList) {
-//				System.out.print(attribute + " = " + gt.getAsObject(attribute) + ", ");
-//			}
-//		}
-//		return null;
-//	}
 }
